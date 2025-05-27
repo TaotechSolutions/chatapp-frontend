398b4116fdc6c4436875480c9103fb47b31c5151
Cleve-keem <belloharyourmidey@gmail.com>
2025-05-27 06:38:01 -0500

    merge both branches

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import NotificationBell from "./components/NotificationBell";
 function App() {
   return (
     <div>
-      <h1 className="flex items-center justify-center min-h-screen bg-darkPrimary text-center font-bold text-darkText text-4xl font-poppins">
+      {/* <h1 className="flex items-center justify-center min-h-screen bg-darkPrimary text-center font-bold text-darkText text-4xl font-poppins">
        to Taotech Solutions 
-      </h1>
+      </h1> */}
       <NotificationBell />
     </div>
   );
