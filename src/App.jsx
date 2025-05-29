0f9c8a2919841ea0e3ab521210cc04efc48d4a94
jeffrey <pikinperson509@gmail.com>
2025-05-30 00:17:12 +0100

    completed the theme feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useTheme } from "./context/ThemeContext";
+
 function App() {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <>
+    <div className={`flex flex-row items-center justify-center relative `}>
       <h1 className="flex items-center justify-center min-h-screen bg-darkPrimary text-center font-bold text-darkText text-4xl font-poppins">
-        Welcome to Taotech Solutions 
+        Welcome to Taotech Solutions
       </h1>
-    </>
+      <button onClick={toggleTheme} className="bg-red-600 text-white">theme</button>
+    </div>
   );
 }
 
---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,37 @@
+import {
+  Children,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const ThemeContext = createContext();
+
+export default function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+
+  function toggleTheme() {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  }
+
+  useEffect(() => {
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        setTheme,
+        toggleTheme
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+export function useTheme() {
+  return useContext(ThemeContext);
+}
