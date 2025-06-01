4761456979d3afbe375c990f1d5aaeb0e3406c55
Cleve-keem <belloharyourmidey@gmail.com>
2025-06-01 00:40:50 -0500

    overwrite App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import NotificationBell from "./components/NotificationBell";
 
 function App() {
   return (
-    
+    <div>
+      <h1 className="flex items-center justify-center min-h-screen bg-darkPrimary text-center font-bold text-darkText text-4xl font-poppins">
+       to Taotech Solutions 
+      </h1>
+    </div>
   );
 }
 
