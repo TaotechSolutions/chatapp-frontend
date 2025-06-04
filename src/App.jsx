3b8c4a1204c120a15da5e405da5bfd48880cabba
Cleve-keem <belloharyourmidey@gmail.com>
2025-06-04 07:51:49 -0500

    added a chatIcon to the head

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import AuthLayout from "./Layouts/AuthLayout";
+import AuthLayout from "./Layouts/AuthLayout"
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Dashboard from "./pages/Dashboard"
