f9228c6aedd54e2c6e0c7e1f28230caba9a0a4f4
Emmanuel Ekpobimi <noble4special247@gmail.com>
2025-06-10 12:27:21 +0100

    Improve button accessibility and CSS consistency
    Adds a pointer cursor to the button for better user feedback and aligns CSS imports and syntax for consistency. Enhances usability and code style uniformity.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,8 @@
-
 export default function Button({ children }) {
   return (
     <button
       //   disabled={!isFormValid()} ${isFormValid() ? "bg-green-600 hover:bg-green-700": "bg-gray-400 cursor-not-allowed"}
-      className={`text-white rounded px-4 py-2 mt-2 transition `}
-    >
+      className={`text-white rounded px-4 py-2 mt-2 transition cursor-pointer `}>
       {children}
     </button>
   );
