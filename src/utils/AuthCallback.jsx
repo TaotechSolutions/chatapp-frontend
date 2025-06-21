2d7add89a54a19567f5648ffb56246d0d0a92d2e
xwaynex <nobodysshinigami@proton.me>
2025-06-21 09:33:40 +0100

    -user request delay to set cookie before that

--- src/utils/AuthCallback.jsx
+++ src/utils/AuthCallback.jsx
@@ -1,17 +1,22 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { setUserFromCookie } from '../features/auth/authActions';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setUserFromCookie } from "../features/auth/authActions";
+import { useNavigate } from "react-router-dom";
 
 function AuthCallback() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
-    dispatch(setUserFromCookie())
-      .unwrap()
-      .then(() => navigate('/dashboard'))
-      .catch(() => navigate('/auth-login'));
+    // Add a small delay to ensure cookie is set
+    const timer = setTimeout(() => {
+      dispatch(setUserFromCookie())
+        .unwrap()
+        .then(() => navigate("/dashboard"))
+        .catch(() => navigate("/auth-login"));
+    }, 100); // 100ms delay
+
+    return () => clearTimeout(timer);
   }, [dispatch, navigate]);
 
   return <p>Authenticating...</p>;
